fb7e1557dde1cb1b4cc072fbf19bd1e51d144859
TomPalacios <tomaspalaciosmulet@gmail.com>
2021-11-26 21:48:30 -0300

    cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const port = process.env.PORT        || 3000;
 const db = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/hellodb';
 const app = express();
+const cors     = require('cors');
 
 const router = require('./routes/api/user');
 
@@ -12,6 +13,8 @@ mongoose
     console.log(`DB connected @ ${db}`);
   })
   .catch(err => console.error(`Connection error ${err}`));
+
+  app.use(cors());
 
   app.use('/api', router);
 
